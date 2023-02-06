90839a897114b485535e783c42e693fc002e4be6
kidygamer <102514637+kidygamer@users.noreply.github.com>
2023-02-06 17:13:44 +0700

    views update

--- services/frontend/src/router/index.js
+++ services/frontend/src/router/index.js
@@ -1,17 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue';
 import RegisterView from '@/views/RegisterView.vue';
+import LoginView from '@/views/LoginView.vue';
+import DashboardView from '@/views/DashboardView.vue';
+import ProfileView from '@/views/ProfileView.vue';
+import NoteView from '@/views/NoteView.vue';
+import EditNoteView from '@/views/EditNoteView.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: HomeView
+    name: "Home",
+    component: HomeView,
   },
   {
     path: '/register',
     name: 'Register',
     component: RegisterView,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: ProfileView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/note/:id',
+    name: 'Note',
+    component: NoteView,
+    meta: { requiresAuth: true },
+    props: true,
+  },
+  {
+    path: '/editnote/:id',
+    name: 'EditNote',
+    component: EditNoteView,
+    meta: { requiresAuth: true },
+    props: true,
   },
 ]
 
