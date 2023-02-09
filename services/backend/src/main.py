d7dfd81fba9d45574ab691c8942021179c6965d7
kidygamer <102514637+kidygamer@users.noreply.github.com>
2023-02-09 09:54:58 +0700

    penyesuaian ip

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # NEW
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["http://10.10.132.214:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -9,7 +9,7 @@ import store from './store'; // New
 const app = createApp(App);
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:5000/';  // the FastAPI backend
+axios.defaults.baseURL = 'http://10.10.132.214:5000/';  // the FastAPI backend
 
 app.use(router);
 app.use(store); // New
